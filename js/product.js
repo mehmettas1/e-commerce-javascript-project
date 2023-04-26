0cee0c6f98a86f34bb883baf0844ad5356547598
mehmet <tas349873@gmail.com>
2023-04-26 22:46:58 +0300

    update

--- js/product.js
+++ js/product.js
@@ -1,79 +1,83 @@
-import {product1} from "./glide.js";
+import { product1 } from "./glide.js";
 
+let products = [];
+let cart = [];
 
-function addToCart(){
-const buttons = [...document.getElementsByClassName("add-to-cart")];
-buttons.forEach((button) =>{
-button.addEventListener("click",function(){
-console.log(button);
-})
-})
+function addToCart() {
+  const buttons = [...document.getElementsByClassName("add-to-cart")];
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      const id = e.target.dataset.id;
+      const findProduct = products.find((product) => product.id === Number(id));
+      console.log(findProduct);
+      cart.push({ ...findProduct, quantity: 1 });
+      localStorage.setItem("cart", JSON.stringify(cart));
+    });
+  });
 }
 
+function productsFunc() {
+  products = localStorage.getItem("products")
+    ? JSON.parse(localStorage.getItem("products"))
+    : [];
+  const productsContainer = document.getElementById("product-list");
 
-
-  function productsFunc(){
-
- const products =  localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")) : [];
-
-    const productsContainer = document.getElementById("product-list")
-   
-
-    let results = "";
-   products.forEach((item) =>{
+  let results = "";
+  products.forEach((item) => {
     results += `
     <li class="product-item glide__slide">
-    <div class="product-image">
-      <a href="#">
-        <img src=${item.img.singleImage} alt="" class="img1">
-        <img src=${item.img.thumbs[1]} alt="" class="img2">
-      </a>
-    </div>
-    <div class="product-info">
-      <a href="$" class="product-title">Analogue Resin Strap</a>
-      <ul class="product-star">
-        <li>
-          <i class="bi bi-star-fill"></i>
-        </li>
-        <li>
-          <i class="bi bi-star-fill"></i>
-        </li>
-        <li>
-          <i class="bi bi-star-fill"></i>
-        </li>
-        <li>
-          <i class="bi bi-star-fill"></i>
-        </li>
-        <li>
-          <i class="bi bi-star-half"></i>
-        </li>
-      </ul>
-      <div class="product-prices">
-        <strong class="new-price">$${item.price.newPrice.toFixed(2)}</strong>
-        <span class="old-price">$${item.price.oldPrice.toFixed(2)}</span>
-      </div>
-      <span class="product-discount">-${item.discount}%</span>
-      <div class="product-links">
-        <button class="add-to-cart" data-id=${item.id}>
-          <i class="bi bi-basket-fill"></i>
-        </button>
-        <button>
-          <i class="bi bi-heart-fill"></i>
-        </button>
+      <div class="product-image">
         <a href="#">
-          <i class="bi bi-eye-fill"></i>
-        </a>
-        <a href="#">
-          <i class="bi bi-share-fill"></i>
+          <img src=${item.img.singleImage} alt="" class="img1">
+          <img src=${item.img.thumbs[1]} alt="" class="img2">
         </a>
       </div>
-    </div>
-  </li>
+      <div class="product-info">
+        <a href="$" class="product-title">${item.name}</a>
+        <ul class="product-star">
+          <li>
+            <i class="bi bi-star-fill"></i>
+          </li>
+          <li>
+            <i class="bi bi-star-fill"></i>
+          </li>
+          <li>
+            <i class="bi bi-star-fill"></i>
+          </li>
+          <li>
+            <i class="bi bi-star-fill"></i>
+          </li>
+          <li>
+            <i class="bi bi-star-half"></i>
+          </li>
+        </ul>
+        <div class="product-prices">
+          <strong class="new-price">$${item.price.newPrice.toFixed(2)}</strong>
+          <span class="old-price">$${item.price.oldPrice.toFixed(2)}</span>
+        </div>
+        <span class="product-discount">-${item.discount}%</span>
+        <div class="product-links">
+          <a href="#" class="add-to-cart" data-id=${item.id}>
+            <i class="bi bi-basket-fill"></i>
+          </a>
+          <button>
+            <i class="bi bi-heart-fill"></i>
+          </button>
+          <a href="#">
+            <i class="bi bi-eye-fill"></i>
+          </a>
+          <a href="#">
+            <i class="bi bi-share-fill"></i>
+          </a>
+        </div>
+      </div>
+    </li>
     `;
     productsContainer.innerHTML = results;
-    addToCart()
-   })
-   product1()
+    addToCart();
+  });
+  product1();
 }
 
 export default productsFunc();
