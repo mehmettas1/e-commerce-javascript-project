5ba861dd551dd5a2e07d1c79656d29393a7b5d5c
mehmet <tas349873@gmail.com>
2023-05-12 12:37:23 +0300

    update

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,25 @@
 import { cart } from "./products.js";
 console.log(cart);
 function displayCartProduct(){
-    const result ="";
+    const cartWrapper = document.querySelector(".cart-wrapper");
+    let result ="";
   cart.forEach((item) =>{
     console.log(item);
-  });
 
+    result +=`
+    <tr class="cart-item">
+            <td></td>
+            <td class="cart-image">
+                <img src=${item.img.singleImage} alt="">
+                <i class="bi bi-x delete-cart"></i>
+            </td>
+            <td>${item.name}</td>
+            <td>$${item.price.newPrice}</td>
+            <td class="product-quantity">${item.quantity}</td>
+            <td class="product-subtotal">$108.00</td>
+    </tr>`
+  });
+cartWrapper.innerHTML = result;
 }
 
 displayCartProduct()
