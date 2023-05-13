9fa0ef0293a95f00a287c67cd1ab3ba5573b756b
mehmet <tas349873@gmail.com>
2023-05-13 12:32:15 +0300

    update

--- js/single-product.js
+++ js/single-product.js
@@ -3,4 +3,13 @@ JSON.parse(localStorage.getItem("productId"))
 :localStorage.setItem("productId",JSON.stringify(1)); 
 
 
-console.log(productId);
+const products = localStorage.getItem("products") ? 
+JSON.parse(localStorage.getItem("products")) 
+:localStorage.setItem("products",JSON.stringify([]));
+
+const findProduct = products.find((item)=> item.id=== Number(productId));
+
+const productTitle = document.querySelector(".product-title");
+
+productTitle.innerHTML =findProduct.name
+
