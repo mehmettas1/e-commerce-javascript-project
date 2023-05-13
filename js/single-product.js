12d90b04056586902640b0525dc769454d7532f4
mehmet <tas349873@gmail.com>
2023-05-13 12:40:21 +0300

    update

--- js/single-product.js
+++ js/single-product.js
@@ -9,7 +9,16 @@ JSON.parse(localStorage.getItem("products"))
 
 const findProduct = products.find((item)=> item.id=== Number(productId));
 
+
+//*  product title */
 const productTitle = document.querySelector(".product-title");
 
 productTitle.innerHTML =findProduct.name
 
+//* product price//
+
+const newPriceDom = document.querySelector(".new-price");
+const oldPriceDom = document.querySelector(".old-price");
+
+newPriceDom.innerHTML = findProduct.price.newPrice.toFixed(2);
+oldPriceDom.innerHTML = findProduct.price.oldPrice.toFixed(2);
