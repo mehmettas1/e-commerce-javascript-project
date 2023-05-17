0c61cba082c84d96d1b96f80eee064383bb15058
mehmet <tas349873@gmail.com>
2023-05-17 21:49:33 +0300

    add to cart

--- js/single-product.js
+++ js/single-product.js
@@ -14,19 +14,19 @@ const products = localStorage.getItem("products")
 
 const findProduct = products.find((item) => item.id === Number(productId));
 
-/* product title */
+//! product title */
 const productTitle = document.querySelector(".product-title");
 
 productTitle.innerHTML = findProduct.name;
 
-/* product price */
+//! product price */
 const newPriceDOM = document.querySelector(".new-price");
 const oldPriceDOM = document.querySelector(".old-price");
 
 newPriceDOM.innerHTML = findProduct.price.newPrice.toFixed(2);
 oldPriceDOM.innerHTML = findProduct.price.oldPrice.toFixed(2);
 
-/* product gallery */
+//! product gallery */
 
 const singleImageDOM = document.querySelector("#single-image");
 
@@ -51,4 +51,10 @@ const productThumbs =document.querySelectorAll(".product-thumb .glide__slide img
 
 productThumbs[0].classList.add("active")
 
-console.log(productThumbs);
+//! add to cart
+
+const cart = localStorage.getItem("cart") ? 
+JSON.parse(localStorage.getItem("cart")) :
+[];
+
+const findCart =cart.find((item) => )
