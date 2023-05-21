f4feb39d8cb41517f71238aeed31fbf03e4f2582
mehmet <tas349873@gmail.com>
2023-05-21 18:12:01 +0300

    search.js

--- js/search.js
+++ js/search.js
@@ -1,20 +1,43 @@
+function productRoute(){
+    const resultItemDOM = document.querySelectorAll(".search-results .result-item");
+    resultItemDOM.forEach((item) =>{
+        item.addEventListener("click", () =>{
+            const id = item.dataset.id;
+            if(id){
+                localStorage.setItem("productId",JSON.stringify(id));
+                window.location.href = "single-product.html";
+            }
+      })
+    })
+}
+
+
+
+
+
+
+
+
+
+
 function searchFunc(data) {
   const searchWrapperDom = document.querySelector(".search-results .results");
   let result = "";
 
   data.forEach((item) => {
     result += `
-    <a href="#" class="result-item">
+    <a href="#" class="result-item " data-id=${item.id}>
             <img src=${item.img.singleImage} class="search-thumb" alt="">
             <div class="search-info">
               <h4>${item.name}</h4>
               <span class="search-sku">SKU: PD0016</span>
-              <span class="search-price">$${item.price.newPrice.toFİxed(2)}</span>
+              <span class="search-price">$${item.price.newPrice.toFixed(2)}</span>
             </div>
           </a>
     `;
   });
   searchWrapperDom.innerHTML = result;
+  productRoute();
 }
 
 export default searchFunc;
