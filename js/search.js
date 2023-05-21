9c5edf32bdd9427cb9e0dde8e9c606a650298d7a
mehmet <tas349873@gmail.com>
2023-05-21 23:00:30 +0300

    update

--- js/search.js
+++ js/search.js
@@ -11,20 +11,11 @@ function productRoute(){
     })
 }
 
-
-
-
-
-
-
-
-
-
-function searchFunc(data) {
+function searchFunc(products) {
   const searchWrapperDom = document.querySelector(".search-results .results");
   let result = "";
 
-  data.forEach((item) => {
+  products.forEach((item) => {
     result += `
     <a href="#" class="result-item " data-id=${item.id}>
             <img src=${item.img.singleImage} class="search-thumb" alt="">
@@ -38,6 +29,33 @@ function searchFunc(data) {
   });
   searchWrapperDom.innerHTML = result;
   productRoute();
+
+  const searchInputDOM = document.querySelector(".search-form");
+  let value ="";
+  let filtered = [];
+  searchInputDOM.addEventListener("input" ,(e)=>{
+  value = e.target.value;
+  value = value.trim().toLowerCase();
+  filtered = products.filter((item) => item.name.trim().toLowerCase().includes(value));
+  let result = "";
+  filtered.forEach((item) =>{
+    result += `
+    <a href="#" class="result-item " data-id=${item.id}>
+            <img src=${item.img.singleImage} class="search-thumb" alt="">
+            <div class="search-info">
+              <h4>${item.name}</h4>
+              <span class="search-sku">SKU: PD0016</span>
+              <span class="search-price">$${item.price.newPrice.toFixed(2)}</span>
+            </div>
+          </a>
+    `;
+  });
+  searchWrapperDom.innerHTML = result;
+  productRoute();
+  })
+  
+
+
 }
 
 export default searchFunc;
