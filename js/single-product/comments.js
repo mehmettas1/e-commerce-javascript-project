201db944f9eccb76e99966ab42caa7d193e07dcc
mehmet <tas349873@gmail.com>
2023-05-21 12:30:17 +0300

    update

--- js/single-product/comments.js
+++ js/single-product/comments.js
@@ -2,13 +2,19 @@ const commentReviewsFunc = function() {
     const commentStarsDom = document.querySelectorAll(".comment-form-rating .star")
     commentStarsDom.forEach((item) =>{
         item.addEventListener("click", ()=>{
-            item.classList.add("active")
+            commentStarsDom.forEach((star) =>star.classList.remove("active"))
+            item.classList.add("active");
         })
     })
+};
 
 
+const addNewCommentFunc = () =>{
+    let commentTextDom = document.getElementById("comment-text")
+}
 function commentFunc() {
   commentReviewsFunc();
+  addNewCommentFunc();
 }
 
 export default commentFunc();
